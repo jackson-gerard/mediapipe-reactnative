194611da97a381e5fc74c1665479cd3511c93d2d
jackson-gerard <jacksongerard@kino-fitness.com>
2026-04-17 13:42:21 -0400

    fix: adding in aspect ratios for android camera

--- android/src/main/java/com/tsmediapipe/fragment/CameraFragment.kt
+++ android/src/main/java/com/tsmediapipe/fragment/CameraFragment.kt
@@ -21,6 +21,7 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -202,6 +203,7 @@ class CameraFragment : Fragment(), PoseLandmarkerHelper.LandmarkerListener {
     try {
       camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
       preview?.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
+      fragmentCameraBinding.viewFinder.scaleType = PreviewView.ScaleType.FIT_CENTER
     } catch (exc: Exception) {
       Log.e(TAG, "Use case binding failed", exc)
     }
